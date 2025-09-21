c0c94668c52b8b14eaf6534917c397329fe2f1a9
wagner <wgprogramador.java@outlook.com>
2025-09-21 13:08:22 -0300

    Implementado docker-compose

--- mspedido/src/main/java/wg/mspedido/config/RabbitMQConfig.java
+++ mspedido/src/main/java/wg/mspedido/config/RabbitMQConfig.java
@@ -4,17 +4,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 	
-
 	public static String EXG_NAME_MARKETPLACE = "exg-producao";
 	public static String QUEUE_PRODUCT_LOG = "queue-controller";
 	public static String RK_PRODUCT_LOG = "pedido";
+	
 	
 	@Bean
 	public Queue queue() {
---
+++ mspedido/src/test/java/wg/mspedido/controllers/test/ControllersPedidoTest.java
@@ -0,0 +1,11 @@
+package wg.mspedido.controllers.test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest()
+public class ControllersPedidoTest {
+
+}
